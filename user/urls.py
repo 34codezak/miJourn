52b1e7d907ea45971b475ab148385d85434bcc94
za..k <zak@kali.za..k>
2025-08-06 19:19:13 +0300

    Changed the titles to 'ThoughtFlow - title_name'

--- user/urls.py
+++ user/urls.py
@@ -16,9 +16,9 @@ urlpatterns = [
     # Password reset URLs - consistent naming
     path('password-reset/',
          auth_views.PasswordResetView.as_view(
-             template_name='registration/password_reset.html',
-             email_template_name='registration/password_reset_email.html',
-             subject_template_name='registration/password_reset_subject.txt'
+             template_name='password_reset.html',
+             email_template_name='password_reset_email.html',
+             subject_template_name='password_reset_subject.txt'
          ),
          name='password_reset'),
     path('password-reset/done/',
